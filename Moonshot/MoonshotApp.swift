a18e71467490ba234c18df28885d12728ea7bd1b
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-25 01:37:56 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mit Sheth on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
